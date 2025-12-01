bce19d2098ecdc67850261d94e72c0c9a2eb8574
ruslanshkhaliev <89189171995@mail.ru>
2025-12-01 17:03:58 +0700

    feat(ui) create query provider

---
+++ src/app/providers/QueryProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import {getQueryClient} from '@lib/react-query';
+import {QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+import {PropsWithChildren} from 'react';
+
+export const QueryProvider = ({ children }: PropsWithChildren) => {
+	const queryClient = getQueryClient()
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools />
+		</QueryClientProvider>
+	)
+}
